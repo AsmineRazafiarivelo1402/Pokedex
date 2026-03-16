0651ed07f8096dc250b065aca4b9c01e86938a0a
Miriasme <johndoe@example.com>
2026-03-16 10:33:00 +0300

    build: initialize project, add all necessary dependency

---
+++ pokedex/src/App.tsx
@@ -0,0 +1,19 @@
+
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+   
+    <h1 className='text-2xl bg-red-600'>Asmine</h1>
+     
+
+      
+    </>
+  )
+}
+
+export default App
